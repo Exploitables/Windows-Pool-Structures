7ab1573ecb1574ed0682ca5aa0799ee56794e9e4
Exploitables <crystalwareprojects@gmail.com>
2022-04-18 21:38:19 -0400

    Added pool structures for 20H2

---
+++ Windows Pool Structures/pool_20H2.h
@@ -0,0 +1,76 @@
+#include <Windows.h>
+
+typedef struct _POOL_HEADER
+{
+	union
+	{
+		struct
+		{
+			unsigned long PreviousSize;
+			unsigned long PoolIndex;
+			unsigned long BlockSize;
+			unsigned long PoolType;
+		};
+		unsigned long Ulong1;
+	};
+
+	unsigned long PoolTag;
+	union
+	{
+		unsigned short AllocatorBackTraceIndex;
+		unsigned short PoolTagHash;
+	};
+} POOL_HEADER, * PPOOL_HEADER;
+
+typedef struct _OBJECT_HEADER
+{
+	unsigned long long PointerCount;
+	union
+	{
+		unsigned long long HandleCount;
+		void* NextToFree;
+	};
+
+	void* Lock;
+	unsigned char TypeIndex;
+	union
+	{
+		unsigned char TraceFlags;
+		unsigned long DbgRefTrace;
+		unsigned long DbgTracePermanent;
+	};
+
+	unsigned char InfoMask;
+	union
+	{
+		unsigned char Flags;
+		unsigned long NewObject;
+		unsigned long KernelObject;
+		unsigned long KernelOnlyAccess;
+		unsigned long ExclusiveObject;
+		unsigned long PermanentObject;
+		unsigned long DefaultSecurityQuota;
+		unsigned long SingleHandleEntry;
+		unsigned long DeletedInline;
+	};
+
+	unsigned long Reserved;
+	union
+	{
+		unsigned long long* ObjectCreateInfo;
+		unsigned long long* QuotaBlockCharged;
+	};
+
+	unsigned long long* SecurityDescriptor;
+	void* Body;
+} OBJECT_HEADER, * POBJECT_HEADER;
+
+typedef struct _OBJECT_HEADER_QUOTA
+{
+	unsigned long PagedPoolCharge;
+	unsigned long NonPagedPoolCharge;
+	unsigned long SecurityDescriptorCharge;
+	unsigned long Reserved1;
+	unsigned long long SecurityDescriptorQuotaBlock;
+	unsigned long Reserved2;
+} OBJECT_HEADER_QUOTA, * POBJECT_HEADER_QUOTA;
